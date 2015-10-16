cd1b402ed1447eca21810cf57664afabbb7c42d7
GG7190 <greengirlsrobotics@gmail.com>
2015-10-15 21:02:33 -0500

    extends robot hardware

--- src/main/java/com/greengirls/chinaAutonomous/RampToParking.java
+++ src/main/java/com/greengirls/chinaAutonomous/RampToParking.java
@@ -1,7 +1,10 @@
 package com.greengirls.chinaAutonomous;
 
+import com.greengirls.RobotHardware;
+
 /**
  * Created by Dell User on 10/15/2015.
  */
-public class RampToParking {
+public class RampToParking extends RobotHardware {
+
 }
