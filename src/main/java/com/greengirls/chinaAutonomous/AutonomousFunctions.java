1f28fff633f9518cf9774f7db47599456a0eb3e1
GG7190 <greengirlsrobotics@gmail.com>
2015-10-18 16:08:43 -0500

    Creation of Autonomous Functions

---
+++ src/main/java/com/greengirls/chinaAutonomous/AutonomousFunctions.java
@@ -0,0 +1,20 @@
+package com.greengirls.chinaAutonomous;
+
+import com.greengirls.RobotHardware;
+
+/**
+ * Created by Dell User on 10/18/2015.
+ */
+public class AutonomousFunctions extends RobotHardware{
+
+    /*
+	 *
+	 *
+	 *
+	 */
+   public void Offramp() {
+        //start faceing backword
+    }
+
+}
+
